1f471f364d90b44e7a4b7451a594b2317730c86d
Dhruvin Shah <dhruvinshah16@gmail.com>
2021-04-26 13:48:03 +0530

    Get List of all PowerApps using PowerShell

---
+++ PowerApps- PowerShell/Export List of PowerApps using PS.ps1
@@ -0,0 +1,9 @@
+
+
+Add-PowerAppsAccount
+Get-AdminPowerApp 
+Get-PowerApp
+Get-PowerAppEnvironment
+Get-AdminPowerApp -EnvironmentName 536e80e7-103a-4b3e-b22d-92186d96010f
+Get-AdminPowerApp | Export-Csv -Path '.\PSPowerAppsExport.csv'
+Get-PowerApp | Export-Csv -Path '.\PSPowerAppsExportUSER.csv'
